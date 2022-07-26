f97a17c4e6a5c82e97b817ef344efdfd9e214aa9
JuanMenezes <juan.menezes@outlook.com>
2022-07-26 17:39:07 -0300

    atualizando o readme e main

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Inicializando o repositorio do app template para testes
+# Criando um app template em flutter para implementação e homologação de firebase analytics
+=============
+Autor: `Juan Menezes`
+Hoje estou para falar especificamente sobre Flutter, Firebase Analytics e até um pouco da integração disso tudo com o GA4, irei passar rapidamente pela criação de um app que irá testar nossos eventos até a homologação deles e como visualizar isso na nossa propriedade do ga4, antes de mais nada segue a documentação do plugin oficial da google e mantido em conjunto pela comunidade:
+[doc](https://pub.dev/documentation/firebase_analytics/latest/firebase_analytics/FirebaseAnalytics-class.html)
+Obs.: Caso você já seja mais experiente com tudo isso que eu citei acima e só tem interesse no flutter com firebase você pode pular para o final.
+
+Iremos começar pelo início obviamente que é “mas o que diabos é flutter?”, bom a google tem uma maneira pomposa mas direta de explicar: **“Flutter é o kit de ferramentas de IU do Google para criar aplicativos bonitos e nativamente compilados para dispositivos móveis, web, desktop e incorporados a partir de uma única base de código.”**.
+
+Entrando no campo de qual a principal diferença entre uma coleta em flutter e nativos eu diria que é praticamente nenhuma, diferente de outros tipos de tecnologia cross-plataform do mercado o flutter é mantido pela própria google, com isso trás muitas soluções otimizadas para os serviços da mesma como é o caso do firebase analytics. Sua implementação é muito similar a de nativos, assim como seu desempenho. Sendo assim, a sua principal vantagem é poder usar apenas um código para todas as plataformas.
--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,159 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(MyApp());
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'tabs_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'App Template Coleta',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      navigatorObservers: <NavigatorObserver>[observer],
+      home: MyHomePage(
+        title: 'App Template Coleta',
+        analytics: analytics,
+        observer: observer,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({
+    Key? key,
+    required this.title,
+    required this.analytics,
+    required this.observer,
+  }) : super(key: key);
 
   final String title;
+  final FirebaseAnalytics analytics;
+  final FirebaseAnalyticsObserver observer;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _message = '';
 
-  void _incrementCounter() {
+  void setMessage(String message) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _message = message;
     });
+  }
+
+  Future<void> _viewItemList() async {
+    await widget.analytics.logEvent(
+      name: 'view_item_list',
+      parameters: {
+        'screenName': "/view_item_list/array_items",
+        'items': [
+          {
+            'item_id': '12345678',
+            'item_name': 'sofa',
+            'item_category': 'moveis',
+            'item_variant': 'preto',
+            'item_brand': 'tokstok',
+            'price': '2132.47',
+            'currency': 'brl',
+            'index': '1'
+          },
+          {
+            'item_id': '87654321',
+            'item_name': 'cama',
+            'item_category': 'quarto',
+            'item_variant': 'azul',
+            'item_brand': 'magalu',
+            'price': '1567.98',
+            'currency': 'brl',
+            'index': '2'
+          }
+        ],
+      },
+    );
+  }
+
+  Future<void> _purchase() async {
+    await widget.analytics.logEvent(
+      name: 'purchase',
+      parameters: {
+        'screenName': "/purchase",
+        'transaction_id': '123456',
+        'items': [
+          {
+            'item_id': '12345678',
+            'item_name': 'sofa',
+            'item_category': 'moveis',
+            'item_variant': 'preto',
+            'item_brand': 'tokstok',
+            'price': '2132.47',
+            'currency': 'brl',
+            'index': '1'
+          },
+          {
+            'item_id': '87654321',
+            'item_name': 'cama',
+            'item_category': 'quarto',
+            'item_variant': 'azul',
+            'item_brand': 'magalu',
+            'price': '1567.98',
+            'currency': 'brl',
+            'index': '2'
+          }
+        ],
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          MaterialButton(
+            onPressed: _viewItemList,
+            child: const Text('View Item List'),
+          ),
+          MaterialButton(
+            onPressed: _purchase,
+            child: const Text('Purchase'),
+          ),
+          Text(_message,
+              style: const TextStyle(color: Color.fromARGB(255, 0, 155, 0))),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute<TabsPage>(
+              settings: const RouteSettings(name: TabsPage.routeName),
+              builder: (BuildContext context) {
+                return TabsPage(widget.observer);
+              }));
+        },
+        child: const Icon(Icons.tab),
+      ),
     );
   }
 }
